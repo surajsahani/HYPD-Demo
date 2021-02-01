d26a1e3ff9dcbfcc1d59d20467826598ab0ba049
user name <user email>
2021-02-01 13:15:52 +0530

    first commit

---
+++ app/src/main/java/martialcoder/surajsahani/hypddemo/MainActivity.kt
@@ -0,0 +1,21 @@
+package martialcoder.surajsahani.hypddemo
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.sax.StartElementListener
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        supportActionBar?.hide()
+    }
+
+    fun newtest(view: View) {
+        val intent = Intent(this@MainActivity,test::class.java)
+        startActivity(intent)
+    }
+}
